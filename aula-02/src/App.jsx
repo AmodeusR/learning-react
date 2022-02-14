3ed785fac0ad90beb61eb916c39bf9949a96ed9a
Ricardo Magalhães <ricardogleiss@hotmail.com>
2022-02-14 00:00:36 -0300

    Aula 02 - Preparação dos arquivos

---
+++ aula-02/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+import Header from "./components/Header";
+import Nav from "./components/Nav";
+import Footer from "./components/Footer";
+import Home from "./components/Home";
+import NewPost from "./components/NewPost";
+import PostPage from "./components/PostPage";
+import About from "./components/About";
+import Missing from "./components/Missing";
+import { Route, Switch, useHistory } from "react-router-dom";
+
+function App() {
+
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
